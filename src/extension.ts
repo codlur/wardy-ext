1b506d7e1e57888cd73f54aef1093ce110bd8241
domedomains <acodlur@gmail.com>
2026-06-28 12:36:20 +0530

    Add sidebar view with Wardy icon

--- src/extension.ts
+++ src/extension.ts
@@ -1,9 +1,28 @@
 import * as vscode from 'vscode';
 
+class WardyProvider implements vscode.TreeDataProvider<{ label: string }> {
+  getTreeItem(element: { label: string }): vscode.TreeItem {
+    return {
+      label: element.label,
+      collapsibleState: vscode.TreeItemCollapsibleState.None,
+      command: {
+        command: 'wardy.helloWorld',
+        title: 'Hello World',
+      },
+    };
+  }
+
+  getChildren(): { label: string }[] {
+    return [{ label: 'Click me' }];
+  }
+}
+
 export function activate(context: vscode.ExtensionContext) {
   let disposable = vscode.commands.registerCommand('wardy.helloWorld', () => {
     vscode.window.showInformationMessage('Hello World from wardy!');
   });
+
+  vscode.window.registerTreeDataProvider('wardy.sidebar', new WardyProvider());
 
   context.subscriptions.push(disposable);
 }
